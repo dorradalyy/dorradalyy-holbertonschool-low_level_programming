7a7fc09cbd8986ac73948d79bac7fed700dbe983
dorradalyy <dorradaly01@gmail.com>
2020-02-05 16:07:35 +0100

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 #include <unisd.h>
-
 /**
  * main - run all function
  *
- * Description: Change standart error message
+ * Description: fonction manaarfch
  * Return: Return 1 and exit
  */
 int main(void)
 {
 	int l;
 
-	l =
-	 sizeof("and that piece of art is useful" - Dora Korpar, 2015-10-19");
-	
-	write(2,
-	      "and that piece of art is useful - Dora korpar, 2015-10-19\n", l);
+	l =sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\- Dora korpar, 2015-10-19\n", l);
 	return (1);
 }
+
 
