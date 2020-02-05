c80fbcb931b5e65d66cf5f2e4c2fb950d6176708
dorradalyy <dorradaly01@gmail.com>
2020-02-05 16:11:02 +0100

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+#include<string.h>
 #include <unisd.h>
 /**
  * main - run all function
- *
- * Description: fonction manaarfch
+ * 
  * Return: Return 1 and exit
  */
 int main(void)
@@ -11,7 +11,7 @@ int main(void)
 	int l;
 
 	l =sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	write(2, "and that piece of art is useful\- Dora korpar, 2015-10-19\n", l);
+	write(2 , "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", l);
 	return (1);
 }
 
