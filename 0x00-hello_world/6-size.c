e2e1c2d5f74992aec54a73f49b983628b819f605
dorradalyy <dorradaly01@gmail.com>
2020-02-05 15:23:54 +0100

    azerty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,11 @@
  * Return: 0
  */
 int main(void)
-{       printf("Size of a char: %ld byte(s)" sizeof(charType)\n);
-        printf("Size of an int: %ld byte(s)" sizeof(intTYpe)\n);
-        printf("Size of a long int: %ld byte(s)" sizeof(long intType)\n);
-        printf("Size of a long long int: %ld byte(s)" sizeof(long long intType)\n);
-        printf("Size of a float: %ld byte(s)" sizeof (floatType)\n);
-    return (0);
+{
+printf("Size of a char: %ld byte(s)" sizeof(charType)\n);
+printf("Size of an int: %ld byte(s)" sizeof(intTYpe)\n);
+printf("Size of a long int: %ld byte(s)" sizeof(long intType)\n);
+printf("Size of a long long int: %ld byte(s)" sizeof(long long intType)\n);
+printf("Size of a float: %ld byte(s)" sizeof (floatType)\n);
+return (0);
 }
