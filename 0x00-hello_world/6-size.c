034cb9ba4752c5ac57be0576becf4d68d909897e
dorradalyy <dorradaly01@gmail.com>
2020-02-05 15:15:21 +0100

    azerty

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+int main(void)
+{       printf("Size of a char: %ld byte(s)" sizeof(char)\n");
+        printf("Size of an int: %ld byte(s)" sizeof(init)\n");
+        printf("Size of a long int: %ld byte(s)" sizeof(float)\n");
+        printf("Size of a long long int: %ld byte(s)" sizeof(double)\n");
+        printf("Size of a float: %ld byte(s)" sizeof (intType)\n");
+    return (0);
+}
