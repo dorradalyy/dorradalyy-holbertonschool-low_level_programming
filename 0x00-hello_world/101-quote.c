4ef5af8eb05ef96cffdead0ae7b122e2d426ea05
dorradalyy <dorradaly01@gmail.com>
2020-02-05 16:12:37 +0100

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int l;
 
-	l =sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	l =strln("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	write(2 , "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", l);
 	return (1);
 }
