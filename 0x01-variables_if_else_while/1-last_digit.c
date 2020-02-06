7a1c741af8d7c1d3e78d90cbc1d0cc2e11014c86
dorradalyy <dorradaly01@gmail.com>
2020-02-06 15:17:18 +0100

    azerty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
  * main - Entry Point
  *
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -17,7 +17,7 @@ o = n % 10;
 /* your code goes there */
 if (o > 5)
 {
-printf("Last digit of %d is %d  and is greater than 5\n", n, o);
+printf("Last digit of %d is %d and is greater than 5\n", n, o);
 }
 else if (o == 0)
 {
