23580e82bd006650bb46b2c516d4f286d03eb965
dorradalyy <dorradaly01@gmail.com>
2020-02-06 13:23:37 +0100

    azerty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 5)
-		printf("%i and is greater than 5/n", n);
+		printf("%i and is greater than 5\n", n);
 	else if (n == 0)
-		printf("%i and is 0/n", n);
+		printf("%i and is 0\n", n);
 	else
-		printf("%i and is less than 6 and not 0/n", n);
+		printf("%i and is less than 6 and not 0\n", n);
 	return (0);
 }
