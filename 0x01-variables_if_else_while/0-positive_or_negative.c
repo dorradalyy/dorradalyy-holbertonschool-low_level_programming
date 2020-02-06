88f38c2b71f3f2ea04aa1f8ce4d1db583d8c3999
dorradalyy <dorradaly01@gmail.com>
2020-02-06 12:54:32 +0100

    azerty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,19 +2,21 @@
 #include <time.h>
 #include <stdio.h>
 /**
-/* more headers goes there */
-/* betty style doc for function main goes there */
+* main - main block
+* Return: 0
 */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand () - RAND_MAX / 2;	
-if (n > 0)
-printf("%i is positive/n", n);
-else if (n == 0)
-printf("%i is zero/n", n);
-else
-printf("%i is negative/n", n);
-return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%i is positive/n", n);
+	else if (n < 0)
+		printf("%i is negative/n", n);
+	else
+		printf("%i is zero/n", n);
+	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,19 +2,21 @@
 #include <time.h>
 #include <stdio.h>
 /**
-/* more headers goes there */
-/* betty style doc for function main goes there */
+* main - block main
+* Return: 0
 */
 int main(void)
 {
-int n;
-stand(time(0));
-n = rand () - RAND_MAX / 2;
-if(n > 5)
-printf("%i and is greater than 5/n", n);
-else if(n == 0)
-printf("%i and is 0/n", n);
-else
-printf("%i and is less than 6 and not 0/n", n);
-return (0);
+	int n;
+
+	stand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 5)
+		printf("%i and is greater than 5/n", n);
+	else if (n == 0)
+		printf("%i and is 0/n", n);
+	else
+		printf("%i and is less than 6 and not 0/n", n);
+	return (0);
 }
