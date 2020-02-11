bc8c8395ffe6e07f38175bacadab09ae0881bb0e
dorradalyy <dorradaly01@gmail.com>
2020-02-11 11:40:00 +0100

    azerty

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+ void print_alphabet_x10(void)
+{
+char i;
+int x;
+for(x = 0 ; x < 10 ; x++)
+{
+for (i = 'a' ; i <= 'z' ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
+}
