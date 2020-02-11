722023322eecfabae4c77783a726d9bbcf49afbf
dorradalyy <dorradaly01@gmail.com>
2020-02-11 16:06:18 +0100

    azerty

---
+++ 0x02-functions_nested_loops/7-last_digit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* print_last_digit - check the code for Holberton School students.
+* @n : Chek the codes
+* Return: Always 0.
+*/
+int print_last_digit(int n)
+{
+if (n < 0)
+{
+_putchar((n % 10) * -1 + '0');
+return ((n % 10) * -1);
+}
+else
+{
+_putchar(n % 10 + '0');
+return (n % 10);
+}
+}
