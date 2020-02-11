ec2c5ac3442eb997ed91888bbbc093e5bf4270b9
dorradalyy <dorradaly01@gmail.com>
2020-02-11 11:01:56 +0100

    azerty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * main - check the code
  *
@@ -8,7 +7,7 @@
 void print_alphabet(void)
 {
 char x;
-for (char x = 'a' ; x <= 'z' ; x++)
+for (x = 'a' ; x <= 'z' ; x++)
 {
 _putchar(x);
 }
