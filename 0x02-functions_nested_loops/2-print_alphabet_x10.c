fac5060486c45beb1fafd8b78b9e8ff24f5d6702
dorradalyy <dorradaly01@gmail.com>
2020-02-11 11:58:38 +0100

    azerty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,9 @@ void print_alphabet_x10(void)
 {
 char i;
 int x;
-
-for (x = 0 ; x < 10 ; x++)
+for (x = 0; x < 10; x++)
 {
-for (i = 'a' ; i <= 'z' ; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
