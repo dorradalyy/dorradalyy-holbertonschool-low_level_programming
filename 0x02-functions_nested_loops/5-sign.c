0ecbf8c7a669dba9e0a7752e7caf681ba20ebb87
dorradalyy <dorradaly01@gmail.com>
2020-02-11 14:38:06 +0100

    azerty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar(+);
+_putchar('+');
 return (1);
 }
-if else(n = 0)
+if else(n < 0)
 {
-_putchar(0);
-return (0);
+_putchar('-');
+return (-1);
 }
 else
 {
-_putchar(-);
-return (-1);
+_putchar('0');
+return (0);
 }
 }
