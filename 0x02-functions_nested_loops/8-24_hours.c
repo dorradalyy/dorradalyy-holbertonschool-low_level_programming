5d20dc056f36c508eccb1093c08259e2db525ebc
dorradalyy <dorradaly01@gmail.com>
2020-02-11 17:59:43 +0100

    azerty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,13 +2,14 @@
 /**
 * jack_bauer - check the code for Holberton School students.
 *
-* Return: Always 0.
+* Return: nothing.
 */
 void jack_bauer(void)
 {
 int h, m;
 for (h = 0; h < 24; h++)
-for (m = 0; m < 60; m++)
+{
+    for (m = 0; m < 60; m++)
 {
 _putchar(h / 10 + '0');
 _putchar(h % 10 + '0');
@@ -16,5 +17,6 @@ _putchar(':');
 _putchar(m / 10 + '0');
 _putchar(m % 10 + '0');
 _putchar('\n');
+}
 }
 }
