e1bce699f3c22bca47c1c8c7b7d945e52ec0af9d
dorradalyy <dorradaly01@gmail.com>
2020-02-11 13:39:32 +0100

    azerty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,18 @@
+
 #include "holberton.h"
 /**
 * _islower - check the code for Holberton School students.
-* @c : Chek the code
+* @c : Chek the codes
 * Return: Always 0.
 */
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
-Return(1);
+return(1);
 }
 else
 {
-Return(0);
+return(0);
 }
 }
