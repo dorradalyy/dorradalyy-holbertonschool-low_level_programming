b2421ea36bd5bad98da68faabc328473b7ec9a17
dorradalyy <dorradaly01@gmail.com>
2020-02-11 21:23:12 +0100

    azerty

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,46 +1,47 @@
 #include <stdio.h>
+/**
+* print_to_98 - check the code
+* @n : int
+* Return: Always 0.
+**/
 
-void x(int n )
+void print_to_98(int n)
 {
-    int i;
-    if (n < 98)
-    {
-        for (i = n ; i < 98 ; i++)
-        {
-            if (i == 98)
-            {
-                printf("%d", i);
-            }
-            printf("%d", i);
-            else
-            {
-                printf("%d, ", i);
-            }
-            printf('\n');
-        }
-
-        
-    }
-    else if (n > 98)
-    {
-        for (i = n ; i > 98 ; i--)
-        {
-            if (i == 98)
-            {
-                printf("%d", i);
-            }
-            printf("%d", i);
-            else
-            {
-                printf("%d, ", i);
-            }
-            printf('\n');
-        }
-
-        
-    }
-    else
-    {
-        printf("%d", n);
-    }
+int i;
+if (n < 98)
+{
+for (i = n ; i < 98 ; i++)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+printf("%d", i);
+else
+{
+printf("%d, ", i);
+}
+printf('\n');
+}
+}
+else if (n > 98)
+{
+for (i = n ; i > 98 ; i--)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+printf("%d", i);
+else
+{
+printf("%d, ", i);
+}
+printf('\n');
+}
+}
+else
+{
+printf("%d", n);
+}
 }
