1564354b15e2bad9b40b7e1754029483504f6716
dorradalyy <dorradaly01@gmail.com>
2020-02-11 14:05:03 +0100

    azerty

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* _isalpha - check the code for Holberton School students.
+* @c : Chek the codes
+* Return: Always 0.
+*/
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
