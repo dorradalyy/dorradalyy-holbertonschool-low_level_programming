c3fb4a2ba420f78d2ad1372a96d03ee4c16b81c7
dorradalyy <dorradaly01@gmail.com>
2020-02-11 14:54:07 +0100

    azerty

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+*_abs - check the code for Holberton School students.
+* @n : Chek the codes
+* Return: Always 0.
+*/
+int _abs(int n)
+{
+if (n < 0)
+_putchar(n * -1);
+return (0);
+}
