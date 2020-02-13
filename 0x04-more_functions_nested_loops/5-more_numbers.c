eb6089f30f2a1982146f63802c68c1d8d9c59946
dorradalyy <dorradaly01@gmail.com>
2020-02-13 20:28:07 +0100

    qwerty

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * more_numbers - check the code for Holberton School students.
  *
@@ -10,13 +9,13 @@ void more_numbers(void)
 int i, j;
 for (i = 0; i < 10; i++)
 {
-for (j = 0 ; j <= 14 ; j++)
+for (j = 0; j <= 14 ; j++)
 {
-if (j > 9)
+if (j >= 10)
 {
 _putchar(j / 10 + '0');
 }
-putchar(j % 10 + '0');
+_putchar(j % 10 + '0');
 }
 _putchar('\n');
 }
