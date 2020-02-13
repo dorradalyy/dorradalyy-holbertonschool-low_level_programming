fb261d41120b869faf09152a1ee80e4b00d2117c
dorradalyy <dorradaly01@gmail.com>
2020-02-13 19:20:40 +0100

    qwerty

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,17 @@ void more_numbers(void)
 int i;
 for (i = 0; i < 10; i++)
 {
+int j;
 for (j = 0 ; j <= 14 ; j++)
-{
+}
 if (j > 9)
 {
 _putchar(j / 10 + '0');
 }
+{
 _putchar(j % 10 + '0');
 }
+{
 _putchar('\n')
 }
 }
