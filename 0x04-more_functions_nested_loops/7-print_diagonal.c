75b4502d93b37d3f83cf44495cb070a0baa79fdc
dorradalyy <dorradaly01@gmail.com>
2020-02-13 20:56:35 +0100

    qwerty

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,23 @@
 void print_diagonal(int n)
 {
 int i, h;
+if (n > 0)
+{
 for (i = 0; i < n; i++)
 {
 for (h = 0; h < i; h++)
+{
+if (i > 0)
 {
 _putchar(' ');
 }
 }
 _putchar('\\');
 _putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
