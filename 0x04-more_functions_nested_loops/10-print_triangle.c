112dc414da327d824f3099b19e12520efc4ae92d
dorradalyy <dorradaly01@gmail.com>
2020-02-13 22:40:54 +0100

    qwerty

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 
 /**
- * rint_square - check the code for Holberton School students.
- * @size : nember
+ * print_triangle - check the code for Holberton School students.
+ * @size : int
  * Return: Always 0.
  */
 void print_triangle(int size)
 {
-int d;
+int d; 
 int g;
 if (size > 0)
 {
 for (d = 0; d < size; d++)
 {
-for (g = 0; g < size g++)
+for (g = 0; g < size; g++)
 {
 if (g < size - d - 1)
 {
@@ -29,6 +29,6 @@ _putchar('\n');
 }
 else
 {
-_putchar('\n')
+_putchar('\n');
 }
 }
