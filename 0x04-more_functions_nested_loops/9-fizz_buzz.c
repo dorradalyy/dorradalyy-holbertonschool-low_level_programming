1efe41cc2a1aee8f1f7085f77e3098711a674f33
dorradalyy <dorradaly01@gmail.com>
2020-02-14 21:58:23 +0100

    qwerty

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int d;
+for (d = 1; d <= 100; d++)
+{
+if ((d % 3) + (d % 5) == 0)
+{
+printf("FizzBuzz");
+}
+else if (d % 3 == 0)
+{
+printf("Fizz");
+}
+else if (d % 5 == 0)
+{
+printf("Buzz");
+}
+else
+printf("%d", d);
+}
+printf("\n");
+return(0);
