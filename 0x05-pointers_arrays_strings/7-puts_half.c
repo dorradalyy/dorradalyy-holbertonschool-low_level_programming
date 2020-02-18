ab8e4b8968d246754292244bf2d8caef83680ed6
dorradalyy <dorradaly01@gmail.com>
2020-02-18 23:03:52 +0100

    qwerty

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,3 +7,14 @@
  * Return: Always 0.
  */
 void puts_half(char *str)
+{
+int d;
+for (d = 0; str[d] != '\0'; d++)
+;
+d++;
+for (d /= 2; str[d] != '\0'; d++);
+{
+_putchar(str[d]);
+}
+_putchar('\n');
+}
