2ed307344b0ffec994a738dae9af244e60ea9557
dorradalyy <dorradaly01@gmail.com>
2020-02-18 23:43:03 +0100

    qwerty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,7 +3,17 @@
 
 /**
  * _strcpy - check the code for Holberton School students.
- *
+ * @dest: int
+ * @src: int
  * Return: Always 0.
  */
 char *_strcpy(char *dest, char *src)
+{
+int g;
+for (g = 0 ; src[g] != '\0' ; g++)
+{
+dest[g] = src[g];
+}
+dest[g] = '\0';
+return (dest);
+}
