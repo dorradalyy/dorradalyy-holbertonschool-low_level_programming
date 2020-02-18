e07a0cd53cbad0301ce604e37753dcfdf518454c
dorradalyy <dorradaly01@gmail.com>
2020-02-18 22:47:13 +0100

    qwerty

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,11 @@
  */
 void puts2(char *str)
 {
-        int d;
-        
-        for (d = 0; str[d] != '\0'; d++)
-        {
-                if (d % 2 == 0)
-                        _putchar(str[d]);
-        }
-        _putchar('\n');
+int d;
+for (d = 0; str[d] != '\0'; d++)
+{
+if (d % 2 == 0)
+_putchar(str[d]);
+}
+_putchar('\n');
 }
