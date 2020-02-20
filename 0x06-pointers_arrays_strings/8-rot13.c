6f1102d79f47fb972c1d978f09b5778187f54d0e
dorradalyy <dorradaly01@gmail.com>
2020-02-20 23:34:15 +0100

    azerty

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rot13 - check the code for Holberton School students.
+ * @s: char
+ * Return: Always 0.
+ */
+char *rot13(char *s)
+{
+int i;
+char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+char storel[] = "nopqrstuvwxyzabcdefghijklm";
+for (i = 0; s[i] != '\0'; i++)
+{
+if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+{
+s[i] = (s[i] - 65 > 25) ?
+storel[s[i] - 97] : storeh[s[i] - 65];
+}
+}
+return (s);
+}
