a8d86bbc17aab583d803df82969749894f141d87
dorradalyy <dorradaly01@gmail.com>
2020-02-20 21:55:03 +0100

    azerty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-for (i = 0; s1[i] != '\0'; i++)
+int d;
+for (d = 0; s2[d] != '\0'; d++)
 {
-if (s2[i] != s1[i])
+if (s1[d] != s2[d])
 }
-return (s2[i] - s1[i]);
+return (s1[d] - s2[d]);
 }
+return (0);
