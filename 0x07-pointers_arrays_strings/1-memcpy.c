a76a1b6e8682631bdff86399c3c313f639110163
dorradalyy <dorradaly01@gmail.com>
2020-02-24 22:36:41 +0100

    qwerty

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,17 @@
 
 /**
  * _memcpy - check the code for Holberton School students.
- *
+ * @src: int
+ * @dest: int
+ * @n: int
  * Return: Always 0.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
