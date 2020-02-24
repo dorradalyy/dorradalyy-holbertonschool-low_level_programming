ec4e936a17afdf19c926bb60f938e7f48a1f6719
dorradalyy <dorradaly01@gmail.com>
2020-02-24 23:26:50 +0100

    qwerty

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,3 +7,17 @@
  * Return: Always 0.
  */
 void print_chessboard(char (*a)[8])
+{
+int i = 0, j = 0;
+while (i < 8)
+{
+while (j < 8)
+{
+_putchar(a[i][j]);
+j++;
+}
+j = 0;
+i++;
+_putchar('\n');
+}
+}
