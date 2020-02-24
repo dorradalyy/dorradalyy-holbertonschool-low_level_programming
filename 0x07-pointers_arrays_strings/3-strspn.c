034d91d755d111ffdfb93d324f3c025409030886
dorradalyy <dorradaly01@gmail.com>
2020-02-24 22:54:51 +0100

    qwerty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,6 +11,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 int dd = 0, gg = 0;
 for (dd = 0; s[dd] != '\0'; dd++)
+{
 for (gg = 0; accept[gg]; gg++)
 {
 if (accept[gg] == s[dd])
@@ -18,9 +19,8 @@ if (accept[gg] == s[dd])
 break;
 }
 if (accept[gg] != s[dd])
-}
-{
 break;
+}
 }
 return (dd);
 }
