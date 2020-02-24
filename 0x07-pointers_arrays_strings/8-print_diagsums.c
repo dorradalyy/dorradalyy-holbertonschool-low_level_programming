32e8d02606bd2fe6f9c725859c7709beb3f86642
dorradalyy <dorradaly01@gmail.com>
2020-02-24 23:34:30 +0100

    qwerty

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,3 +7,12 @@
  * Return: Always 0.
  */
 void print_diagsums(int *a, int size)
+{
+int i = 0, sum_l = 0, sum_r = 0;
+for (; i < size; i++)
+{
+sum_l += *(a + i * size + i);
+sum_r += *(a + (i * size) + (size - 1 - i));
+}
+printf("%i, %i\n", sum_l, sum_r);
+}
