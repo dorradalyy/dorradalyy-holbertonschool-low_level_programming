1a0565feee9817c946c22c9d7eb1f1ae3ff223a1
dorradalyy <dorradaly01@gmail.com>
2020-02-24 22:52:37 +0100

    qwerty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,24 @@
 
 /**
  * _strspn - check the code for Holberton School students.
- *
+ * @s: char
+ * @accept: char
  * Return: Always 0.
  */
 unsigned int _strspn(char *s, char *accept)
+{
+int dd = 0, gg = 0;
+for (dd = 0; s[dd] != '\0'; dd++)
+for (gg = 0; accept[gg]; gg++)
+{
+if (accept[gg] == s[dd])
+{
+break;
+}
+if (accept[gg] != s[dd])
+}
+{
+break;
+}
+return (dd);
+}
