7e3616b9ef9151832ab426f7deee12c36b9d3ff2
dorradalyy <dorradaly01@gmail.com>
2020-02-24 23:08:58 +0100

    qwerty

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,30 @@
 
 /**
  * _strpbrk - check the code for Holberton School students.
- *
+ * @s: char
+ * @accept: char
  * Return: Always 0.
  */
+
 char *_strpbrk(char *s, char *accept)
+
+{
+int dd, gg;
+dd = 0;
+gg = 0;
+while (s[dd] != '\0')
+{
+while (accept[gg] != '\0')
+{
+if (s[dd] == accept[gg])
+{
+break;
+}
+gg++;
+}
+if (accept[gg] != '\0')
+return (s + dd);
+dd++;
+}
+return (0);
+}
