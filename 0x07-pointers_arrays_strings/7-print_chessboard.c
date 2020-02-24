0e7a9fe469a72ef28ee7b555495f62238b98a49c
dorradalyy <dorradaly01@gmail.com>
2020-02-24 23:30:50 +0100

    qwerty

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,21 +3,21 @@
 
 /**
  * print_chessboard - check the code for Holberton School students.
- *
+ * @a: int
  * Return: Always 0.
  */
 void print_chessboard(char (*a)[8])
 {
-int i = 0, j = 0;
-while (i < 8)
+int dd = 0, gg = 0;
+while (dd < 8)
 {
-while (j < 8)
+while (gg < 8)
 {
-_putchar(a[i][j]);
-j++;
+_putchar(a[dd][gg]);
+gg++;
 }
-j = 0;
-i++;
+gg = 0;
+dd++;
 _putchar('\n');
 }
 }
