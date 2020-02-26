0fb58cce66f65558d09fc7d166629bea000b658b
dorradalyy <dorradaly01@gmail.com>
2020-02-26 23:34:23 +0100

    qwerty

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,11 +8,12 @@
  */
 int _pow_recursion(int x, int y)
 {
-int result = x;
 if (y < 0)
 return (-1);
 else if (y == 0)
 return (1);
-result *= _pow_recursion(x, y - 1);
-return (result);
+else if (y == 1)
+return (x);
+
+return (x * _pow_recursion(x, y - 1));
 }
