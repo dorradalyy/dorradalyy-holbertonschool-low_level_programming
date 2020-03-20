c2244006234ed8fa1f165072c706c41844da7856
dorradalyy <dorradaly01@gmail.com>
2020-03-20 20:13:49 +0100

    azerty

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * malloc_checked - check the code for Holberton School students.
+ * @b : int
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+void *i;
+i = malloc(b);
+if (i == NULL)
+exit(98);
+return (i);
+}
